92147017e74fb537baa34dcd08984364894a39f6
terabyte3 <thrizzle@skiff.com>
2022-12-21 20:18:47 -0500

    🎨 set up some more astro stuff + template blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,15 @@
 import { defineConfig } from 'astro/config';
+import prefetch from "@astrojs/prefetch"
+import sitemap from "@astrojs/sitemap"
+import tailwindIntegration from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        prefetch(),
+        sitemap(),
+        tailwindIntegration(),
+        mdx()
+    ]
+});
